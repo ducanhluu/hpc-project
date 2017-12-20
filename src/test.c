ed439a2d9a5a6c10caf17c60af839d3396b63094
ducanhluu <ducanhluu2310@gmail.com>
2017-12-20 09:05:28 +0100

    Update implementation

--- src/test.c
+++ src/test.c
@@ -1,33 +1,42 @@
-// #include <stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-// int* UTIL_genArray(int x, int y, int min, int max){
-//     int* arr_out = malloc(sizeof(int)*x*y);
-//     int i, j;
-//     for (i = 0; i < x; i++) {
-//         for (j = 0; j < y; j++) {
-//             arr_out[x*i+j] = min + rand()%(max-min);
-//         }
-//     }
-//     return arr_out;
-// }
+double **UTIL_genArray(int height, int width) {
+    double **arr_out;
+    int i, j;
 
-// void UTIL_printArray(int x, int y, int* arr){
-//     int i, j;
-//     printf("Array of size %dx%d\n", x, y);
-//     for (i = 0; i < x; i++){
-//         for (j = 0; j < y; j++) {
-//             printf("%d ", arr[x*i+j]);
-//         }
-//         printf("\n");
-//     }
-// }
+    //-- Allocate the array
+	arr_out = (double **)malloc(height * sizeof(double *));
+    for (i = 0; i < height; i++) {
+		arr_out[i] = (double *)malloc(width * sizeof(double));
+	}
+    
+    srand ( time ( NULL));
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            //arr_out[x*i+j] = min + rand()%(max-min);
+            arr_out[i][j] = (double)rand()/RAND_MAX*2.0-1.0;
+        }
+    }
+    return arr_out;
+}
+
+void UTIL_printArray(int height, int width, double** arr){
+    int i, j;
+    printf("Array of size %dx%d\n", height, width);
+    for (i = 0; i < height; i++){
+        for (j = 0; j < width; j++) {
+            printf("%2.5f \t", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 double **UTIL_maxPooling(double **data, int w, int h, int f_size, int stride) {
 	double **output;
     int i, j, k, l;
-    int max;
+    double max;
 
 	//-- Compute size of output
 	int o_h = (h - f_size) / stride + 1;
@@ -50,7 +59,7 @@ double **UTIL_maxPooling(double **data, int w, int h, int f_size, int stride) {
 
     for (i = 0; i < o_h; i++) {
         for (j = 0; j < o_w; j++) {
-            max = 0;
+            max = -100000.0;
             for (k = 0; k < f_size; k++) {
                 for (l = 0; l < f_size; l++) {
                     if (data[i * stride + k][j * stride + l] >= max) {
@@ -63,6 +72,7 @@ double **UTIL_maxPooling(double **data, int w, int h, int f_size, int stride) {
         }
     }
 
+    printf("Output of data of shape (%d, %d), filter_size = %d, stride = %d\n", h, w, f_size, stride);
     for (int i = 0; i< o_h;i++)
     {
         for ( int j = 0;j< o_w;j++)
@@ -79,41 +89,23 @@ double **UTIL_maxPooling(double **data, int w, int h, int f_size, int stride) {
 
 
 int main(int argc, char *argv[]) {
-    int x, y, min, max;
-    if (argc < 5) return 1;
-    x = atoi(argv[1]);
-    y = atoi(argv[2]);
-    min = atoi(argv[3]);
-    max = atoi(argv[4]);
-    // int* arr = UTIL_genArray(x, y, min, max);
-    // UTIL_printArray(x, y, arr);
-    double **data;
-    data = (double **)malloc(4 * sizeof(double *));
-    for (int i = 0; i < 4; i++) {
-		data[i] = (double *)malloc(4 * sizeof(double));
+    int height, width, f_size, stride;
+    if (argc != 5) {
+        printf("./executed_file height width filter_size stride \n");
+        return 1;
     }
     
-    int k = 0;
+    height = atoi(argv[1]);
+    width = atoi(argv[2]);
+    f_size = atoi(argv[3]);
+    stride = atoi(argv[4]);
 
-    for (int i = 0; i < 4; i++) {
-        for (int j = 0; j < 4; j++) {
-            data[i][j] = k;
-            k++;
-        }
-    } 
-   
-    for (int i = 0; i< 4;i++)
-    {
-        for ( int j = 0;j< 4;j++)
-        {
-            printf("%f\t",data[i][j]);
-        }
-        printf("\n");
-    }
+    double** data = UTIL_genArray(height, width);
+    UTIL_printArray(height, width, data);
+    
     printf("\n");
 
-    double **res = UTIL_maxPooling(data, 4, 4, 2, 2);
-
-    //printf("Hello World %f\n", data[1][1]);
+    double **res = UTIL_maxPooling(data, height, width, f_size, stride);
+    
     return 0;
 }
